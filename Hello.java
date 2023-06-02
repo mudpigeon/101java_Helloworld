3764f8086edfde4c6df1fd39d15e24aa4e654a6b
mudPigeon <mudPigeon@outlook.com>
2023-06-02 20:50:44 +0800

    append some bugs

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
 import lib.oop2.Human;
-import lib.std.StdOut;
+// import lib.std.StdOut;
+import java.io.File;
 
 public class Hello {
     public static void main(String[] args) {
-        StdOut.print("Hello world!\n");
-        StdOut.print("----- Test is begining! -----\n");
+
+        Human linee = new Human();
+        linee.sleep();
+        linee.speak("Hello world!");
+
+        File openFile = new File("./doc/qwe.txt");
+        System.out.println(openFile);
     }
 }
