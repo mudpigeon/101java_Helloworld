98335abba072db8afa57d2d52dd85c533769c59c
mudPigeon <mudPigeon@outlook.com>
2023-06-02 20:07:08 +0800

    删除了丢人的测试

--- Hello.java
+++ Hello.java
@@ -5,10 +5,5 @@ public class Hello {
     public static void main(String[] args) {
         StdOut.print("Hello world!\n");
         StdOut.print("----- Test is begining! -----\n");
-
-        Human yumiya = new Human();
-
-        yumiya.speak("哦哦哦，好爽，草我！");
-        yumiya.speak("Yumiya 受不了了，啊————");
     }
 }
